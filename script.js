738cfc7118afb5c5da00f299225c93407e41fd9d
Fexxonn <torrenegradavid@gmail.com>
2024-10-24 02:14:35 -0500

    First Commit

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
+let data = [];
 const container_products = document.getElementById("container_products");
 
 const TargetsAll = (e) => {
   const Card = document.createElement("div");
   Card.className = "Card";
-  Card.setAttribute("onclick", "start_modal()");
+  Card.setAttribute("onclick", `start_modal(${JSON.stringify(e)})`);
   container_products.appendChild(Card);
 
   Card.innerHTML = `
-        <div class="img--container">
-        <img
-          src=${e.image}
-          alt="" />
-          </div>
-        <p>${e.title}</p>
-        <strong>€ ${e.price}</strong>
-
+    <div class="img--container">
+      <img src="${e.image}" alt="" />
+    </div>
+    <p>${e.title}</p>
+    <strong>€ ${e.price}</strong>
   `;
 };
 
@@ -24,19 +22,31 @@ const API = async () => {
 
   information.forEach((e) => {
     TargetsAll(e);
+    data.push(e);
   });
 };
 
 API();
 
 const modal = document.getElementById("modal");
-const start_modal = () => {
-  
-  modal.style.display = "flex"
+const start_modal = (productData) => {
+  modal.innerHTML = `
+    <div class="container__modal">
+      <span id="close" onclick="end_modal()">X</span>
+      <div class="container__modal--img">
+        <img src="${productData.image}" alt="Imagen" />
+      </div>
+      <div class="container__modal--txt">
+        <h2>${productData.title}</h2>
+        <p>${productData.category}</p>
+        <h5>${productData.description || "No disponible"}</h5>
+        <strong>€ ${productData.price}</strong>
+      </div>
+    </div>
+  `;
+  modal.style.display = "flex";
 };
 
 const end_modal = () => {
-
-  modal.style.display = "none"
-}
-
+  modal.style.display = "none";
+};
